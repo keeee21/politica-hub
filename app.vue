2334abd76fb45a76234c3a57b6a2fd3a0e455f8c
keeee21 <keisuke.u211016@gmail.com>
2025-01-09 23:19:34 +0900

    tailwind + fontawesome

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-2xl font-bold text-center">Vue.jsの基礎</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
     '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+  ],
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css',
   ],
 })
