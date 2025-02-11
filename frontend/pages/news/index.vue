dab50c469d4e8f9a90c5221646a007a9a1bc3892
keeee21 <keisuke.u211016@gmail.com>
2025-02-11 22:04:55 +0900

    feat: newsをiframeで閲覧

--- frontend/pages/news/index.vue
+++ frontend/pages/news/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { convISOStringToJST } from '@/utils/date'
 import TheTitle from '@/components/ui/TheTitle.vue'
 import { useGetNews } from '@/composables/news/useGetNews'
 
-const { data, status, error, refresh, clear } = useGetNews()
+const { data, error } = useGetNews()
+const selectedNews = ref<string | null>(null)
+
+const toggleNews = (url: string) => {
+  selectedNews.value = selectedNews.value === url ? null : url
+}
 </script>
 
 <template>
@@ -21,30 +27,28 @@ const { data, status, error, refresh, clear } = useGetNews()
   </div>
 
   <div v-else class="space-y-6">
-      <div
-        v-for="news in data"
-        :key="news.id"
-        class="bg-white rounded-lg shadow-lg mx-6 p-6 border border-gray-200 transition hover:shadow-xl"
-      >
-        <!-- 日付 -->
-        <div class="flex justify-between items-center text-gray-500 text-sm">
-          <span>{{ convISOStringToJST(news.published_at) }}</span>
-        </div>
+    <div
+      v-for="news in data"
+      :key="news.id"
+      class="bg-white rounded-lg shadow-lg mx-6 p-6 border border-gray-200 transition hover:shadow-xl cursor-pointer"
+      @click="toggleNews(news.url)"
+    >
+      <div class="flex justify-between items-center text-gray-500 text-sm">
+        <span>{{ convISOStringToJST(news.published_at) }}</span>
+        <font-awesome-icon
+          :icon="selectedNews === news.url ? ['fas', 'caret-up'] : ['fas', 'caret-down']"
+          class="ml-2 text-gray-400 text-sm"
+        />
+      </div>
+      <p
+        class="block mt-2 text-md font-bold text-blue-600 hover:underline">
+        {{ news.title }}
+      </p>
+      <p class="text-sm text-gray-700 mt-2">{{ news.description }}</p>
 
-        <!-- タイトル -->
-        <a
-          :href="news.url"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="block mt-2 text-md font-bold text-blue-600 hover:underline"
-        >
-          {{ news.title }}
-        </a>
-
-        <!-- 説明 -->
-        <p class="text-sm text-gray-700 mt-2">
-          {{ news.description }}
-        </p>
+      <div v-if="selectedNews === news.url" class="mt-4">
+        <iframe :src="news.url" class="w-full h-[80vh] border rounded-lg" frameborder="0"/>
       </div>
     </div>
+  </div>
 </template>
